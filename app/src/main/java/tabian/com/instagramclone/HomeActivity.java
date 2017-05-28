2cac213283ceafe3b1c627096065bd11f80d4161
Mitch <mitch@tabian.ca>
2017-05-28 12:09:59 -0700

    Source Code

---
+++ app/src/main/java/tabian/com/instagramclone/HomeActivity.java
@@ -0,0 +1,15 @@
+package tabian.com.instagramclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
