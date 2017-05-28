37a7d8091e96bc0316a7f456b4451fb279d797b5
Mitch <mitch@tabian.ca>
2017-05-28 15:15:05 -0700

    part6 - SectionsPagerAdapter (Home Tabs)

--- app/src/main/java/tabian/com/instagramclone/Home/HomeActivity.java
+++ app/src/main/java/tabian/com/instagramclone/Home/HomeActivity.java
@@ -1,11 +1,14 @@
 package tabian.com.instagramclone.Home;
 
 import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
@@ -26,6 +29,26 @@ public class HomeActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: starting.");
 
         setupBottomNavigationView();
+        setupViewPager();
+    }
+
+    /**
+     * Responsible for adding the 3 tabs: Camera, Home, Messages
+     */
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment()); //index 0
+        adapter.addFragment(new HomeFragment()); //index 1
+        adapter.addFragment(new MessagesFragment()); //index 2
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_action_name);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
     }
 
     /**
