69743899065c2b921f99dde9a2aabb5c8f8adc70
Mitch <mitch@tabian.ca>
2017-05-28 12:52:23 -0700

    part3 - Customizing the BottomNavigationView

--- app/src/main/java/tabian/com/instagramclone/HomeActivity.java
+++ app/src/main/java/tabian/com/instagramclone/HomeActivity.java
@@ -4,12 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import tabian.com.instagramclone.Utils.BottomNavigationViewHelper;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: starting.");
 
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
     }
 }
---
+++ app/src/main/java/tabian/com/instagramclone/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,22 @@
+package tabian.com.instagramclone.Utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+/**
+ * Created by User on 5/28/2017.
+ */
+
+public class BottomNavigationViewHelper {
+
+    private static final String TAG = "BottomNavigationViewHel";
+
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+}
