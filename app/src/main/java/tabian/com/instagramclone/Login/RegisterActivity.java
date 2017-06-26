cb5008eed6fd56db1ad0a768b4a289b5aeae99f6
Mitch <mitch@tabian.ca>
2017-06-26 14:45:30 -0700

    part23 - Register New User with Firebase

--- app/src/main/java/tabian/com/instagramclone/Login/RegisterActivity.java
+++ app/src/main/java/tabian/com/instagramclone/Login/RegisterActivity.java
@@ -11,11 +11,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import tabian.com.instagramclone.R;
+import tabian.com.instagramclone.Utils.FirebaseMethods;
 
 /**
  * Created by User on 6/19/2017.
@@ -35,23 +37,55 @@ public class RegisterActivity extends AppCompatActivity {
     //firebase
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseMethods firebaseMethods;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mContext = RegisterActivity.this;
+        firebaseMethods = new FirebaseMethods(mContext);
         Log.d(TAG, "onCreate: started.");
 
         initWidgets();
         setupFirebaseAuth();
+        init();
     }
 
+    private void init(){
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = mEmail.getText().toString();
+                username = mUsername.getText().toString();
+                password = mPassword.getText().toString();
+
+                if(checkInputs(email, username, password)){
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    loadingPleaseWait.setVisibility(View.VISIBLE);
+
+                    firebaseMethods.registerNewEmail(email, password, username);
+                }
+            }
+        });
+    }
+
+    private boolean checkInputs(String email, String username, String password){
+        Log.d(TAG, "checkInputs: checking inputs for null values.");
+        if(email.equals("") || username.equals("") || password.equals("")){
+            Toast.makeText(mContext, "All fields must be filled out.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
     /**
      * Initialize the activity widgets
      */
     private void initWidgets(){
         Log.d(TAG, "initWidgets: Initializing Widgets.");
         mEmail = (EditText) findViewById(R.id.input_email);
+        mUsername = (EditText) findViewById(R.id.input_username);
+        btnRegister = (Button) findViewById(R.id.btn_register);
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         loadingPleaseWait = (TextView) findViewById(R.id.loadingPleaseWait);
         mPassword = (EditText) findViewById(R.id.input_password);
---
+++ app/src/main/java/tabian/com/instagramclone/Utils/FirebaseMethods.java
@@ -0,0 +1,68 @@
+package tabian.com.instagramclone.Utils;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import tabian.com.instagramclone.R;
+
+/**
+ * Created by User on 6/26/2017.
+ */
+
+public class FirebaseMethods {
+
+    private static final String TAG = "FirebaseMethods";
+
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private String userID;
+
+    private Context mContext;
+
+    public FirebaseMethods(Context context) {
+        mAuth = FirebaseAuth.getInstance();
+        mContext = context;
+
+        if(mAuth.getCurrentUser() != null){
+            userID = mAuth.getCurrentUser().getUid();
+        }
+    }
+
+    /**
+     * Register a new email and password to Firebase Authentication
+     * @param email
+     * @param password
+     * @param username
+     */
+    public void registerNewEmail(final String email, String password, final String username){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(mContext, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                        else if(task.isSuccessful()){
+                            userID = mAuth.getCurrentUser().getUid();
+                            Log.d(TAG, "onComplete: Authstate changed: " + userID);
+                        }
+
+                    }
+                });
+    }
+}
