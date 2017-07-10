ba425509bd4c1c47c2219206ed78f820d04053c2
Mitch <mitch@tabian.ca>
2017-07-10 12:52:07 -0700

    part40 - Update User Account Settings

--- app/src/main/java/tabian/com/instagramclone2/Profile/EditProfileFragment.java
+++ app/src/main/java/tabian/com/instagramclone2/Profile/EditProfileFragment.java
@@ -206,6 +206,25 @@ public class EditProfileFragment extends Fragment implements
             //          -submit the new email to the database and authentication
         }
 
+        /**
+         * change the rest of the settings that do not require uniqueness
+         */
+        if(!mUserSettings.getSettings().getDisplay_name().equals(displayName)){
+            //update displayname
+            mFirebaseMethods.updateUserAccountSettings(displayName, null, null, 0);
+        }
+        if(!mUserSettings.getSettings().getWebsite().equals(website)){
+            //update website
+            mFirebaseMethods.updateUserAccountSettings(null, website, null, 0);
+        }
+        if(!mUserSettings.getSettings().getDescription().equals(description)){
+            //update description
+            mFirebaseMethods.updateUserAccountSettings(null, null, description, 0);
+        }
+        if(!mUserSettings.getSettings().getProfile_photo().equals(phoneNumber)){
+            //update phoneNumber
+            mFirebaseMethods.updateUserAccountSettings(null, null, null, phoneNumber);
+        }
     }
 
 
--- app/src/main/java/tabian/com/instagramclone2/Utils/FirebaseMethods.java
+++ app/src/main/java/tabian/com/instagramclone2/Utils/FirebaseMethods.java
@@ -48,6 +48,47 @@ public class FirebaseMethods {
     }
 
     /**
+     * Update 'user_account_settings' node for the current user
+     * @param displayName
+     * @param website
+     * @param description
+     * @param phoneNumber
+     */
+    public void updateUserAccountSettings(String displayName, String website, String description, long phoneNumber){
+
+        Log.d(TAG, "updateUserAccountSettings: updating user account settings.");
+
+        if(displayName != null){
+            myRef.child(mContext.getString(R.string.dbname_user_account_settings))
+                    .child(userID)
+                    .child(mContext.getString(R.string.field_display_name))
+                    .setValue(displayName);
+        }
+
+
+        if(website != null) {
+            myRef.child(mContext.getString(R.string.dbname_user_account_settings))
+                    .child(userID)
+                    .child(mContext.getString(R.string.field_website))
+                    .setValue(website);
+        }
+
+        if(description != null) {
+            myRef.child(mContext.getString(R.string.dbname_user_account_settings))
+                    .child(userID)
+                    .child(mContext.getString(R.string.field_description))
+                    .setValue(description);
+        }
+
+        if(phoneNumber != 0) {
+            myRef.child(mContext.getString(R.string.dbname_user_account_settings))
+                    .child(userID)
+                    .child(mContext.getString(R.string.field_phone_number))
+                    .setValue(phoneNumber);
+        }
+    }
+
+    /**
      * update username in the 'users' node and 'user_account_settings' node
      * @param username
      */
