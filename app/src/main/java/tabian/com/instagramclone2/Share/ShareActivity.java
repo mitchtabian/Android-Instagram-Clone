d001c3ef0727d27f6b9eae8c4c9084cf31e199dd
Mitch <mitch@tabian.ca>
2017-07-10 14:04:04 -0700

    part41 - Verifying Permissions for Sharing

--- app/src/main/java/tabian/com/instagramclone2/Share/ShareActivity.java
+++ app/src/main/java/tabian/com/instagramclone2/Share/ShareActivity.java
@@ -1,8 +1,11 @@
 package tabian.com.instagramclone2.Share;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.health.PackageHealthStats;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -12,6 +15,7 @@ import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 import tabian.com.instagramclone2.R;
 import tabian.com.instagramclone2.Utils.BottomNavigationViewHelper;
+import tabian.com.instagramclone2.Utils.Permissions;
 
 /**
  * Created by User on 5/28/2017.
@@ -19,7 +23,11 @@ import tabian.com.instagramclone2.Utils.BottomNavigationViewHelper;
 
 public class ShareActivity extends AppCompatActivity{
     private static final String TAG = "ShareActivity";
+
+    //constants
     private static final int ACTIVITY_NUM = 2;
+    private static final int VERIFY_PERMISSIONS_REQUEST = 1;
+
 
     private Context mContext = ShareActivity.this;
 
@@ -29,7 +37,60 @@ public class ShareActivity extends AppCompatActivity{
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: started.");
 
-        setupBottomNavigationView();
+        if(checkPermissionsArray(Permissions.PERMISSIONS)){
+
+        }else{
+            verifyPermissions(Permissions.PERMISSIONS);
+        }
+
+        //setupBottomNavigationView();
+    }
+
+    public void verifyPermissions(String[] permissions){
+        Log.d(TAG, "verifyPermissions: verifying permissions.");
+
+        ActivityCompat.requestPermissions(
+                ShareActivity.this,
+                permissions,
+                VERIFY_PERMISSIONS_REQUEST
+        );
+    }
+
+    /**
+     * Check an array of permissions
+     * @param permissions
+     * @return
+     */
+    public boolean checkPermissionsArray(String[] permissions){
+        Log.d(TAG, "checkPermissionsArray: checking permissions array.");
+
+        for(int i = 0; i< permissions.length; i++){
+            String check = permissions[i];
+            if(!checkPermissions(check)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Check a single permission is it has been verified
+     * @param permission
+     * @return
+     */
+    public boolean checkPermissions(String permission){
+        Log.d(TAG, "checkPermissions: checking permission: " + permission);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(ShareActivity.this, permission);
+
+        if(permissionRequest != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "checkPermissions: \n Permission was not granted for: " + permission);
+            return false;
+        }
+        else{
+            Log.d(TAG, "checkPermissions: \n Permission was granted for: " + permission);
+            return true;
+        }
     }
 
     /**
---
+++ app/src/main/java/tabian/com/instagramclone2/Utils/Permissions.java
@@ -0,0 +1,16 @@
+package tabian.com.instagramclone2.Utils;
+
+import android.Manifest;
+
+/**
+ * Created by User on 7/10/2017.
+ */
+
+public class Permissions {
+
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+}
