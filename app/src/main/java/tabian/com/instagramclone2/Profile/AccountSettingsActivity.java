cb06d076e51e313ed61d3fdb0bc61c480a6cea71
Mitch <mitch@tabian.ca>
2017-07-29 14:09:05 -0700

    part57 - Upload new Profile Photo

--- app/src/main/java/tabian/com/instagramclone2/Profile/AccountSettingsActivity.java
+++ app/src/main/java/tabian/com/instagramclone2/Profile/AccountSettingsActivity.java
@@ -78,6 +78,18 @@ public class AccountSettingsActivity extends AppCompatActivity {
     private void getIncomingIntent(){
         Intent intent = getIntent();
 
+        //if there is an imageUrl attached as an extra, then it was chosen from the gallery/photo fragment
+        if(intent.hasExtra(getString(R.string.selected_image))){
+            Log.d(TAG, "getIncomingIntent: New incoming imgUrl");
+            if(intent.getStringExtra(getString(R.string.return_to_fragment)).equals(getString(R.string.edit_profile_fragment))){
+
+                //set the new profile picture
+                FirebaseMethods firebaseMethods = new FirebaseMethods(AccountSettingsActivity.this);
+                firebaseMethods.uploadNewPhoto(getString(R.string.profile_photo), null, 0,
+                        intent.getStringExtra(getString(R.string.selected_image)));
+            }
+        }
+
         if(intent.hasExtra(getString(R.string.calling_activity))){
             Log.d(TAG, "getIncomingIntent: received incoming intent from " + getString(R.string.profile_activity));
             setViewPager(pagerAdapter.getFragmentNumber(getString(R.string.edit_profile_fragment)));
