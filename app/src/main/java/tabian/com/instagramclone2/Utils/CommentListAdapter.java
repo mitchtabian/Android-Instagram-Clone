f8c889b785bda930744d0e81f2e5132649801e8b
Mitch <mitch@tabian.ca>
2017-08-22 14:16:58 -0700

    part77 - Comments ListAdapter part1

---
+++ app/src/main/java/tabian/com/instagramclone2/Utils/CommentListAdapter.java
@@ -0,0 +1,148 @@
+package tabian.com.instagramclone2.Utils;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import tabian.com.instagramclone2.R;
+import tabian.com.instagramclone2.models.Comment;
+
+/**
+ * Created by User on 8/22/2017.
+ */
+
+public class CommentListAdapter extends ArrayAdapter<Comment> {
+
+    private static final String TAG = "CommentListAdapter";
+
+    private LayoutInflater mInflater;
+    private int layoutResource;
+    private Context mContext;
+
+    public CommentListAdapter(@NonNull Context context, @LayoutRes int resource,
+                              @NonNull List<Comment> objects) {
+        super(context, resource, objects);
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mContext = context;
+        layoutResource = resource;
+    }
+
+    private static class ViewHolder{
+        TextView comment, username, timestamp, reply, likes;
+        CircleImageView profileImage;
+        ImageView like;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        final ViewHolder holder;
+
+        if(convertView == null){
+            convertView = mInflater.inflate(layoutResource, parent, false);
+            holder = new ViewHolder();
+
+            holder.comment = (TextView) convertView.findViewById(R.id.comment);
+            holder.username = (TextView) convertView.findViewById(R.id.comment_username);
+            holder.timestamp = (TextView) convertView.findViewById(R.id.comment_time_posted);
+            holder.reply = (TextView) convertView.findViewById(R.id.comment_reply);
+            holder.like = (ImageView) convertView.findViewById(R.id.comment_like);
+            holder.profileImage = (CircleImageView) convertView.findViewById(R.id.comment_profile_image);
+
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        //set the comment
+        holder.comment.setText(getItem(position).getComment());
+
+        //set the timestamp difference
+        String timestampDifference = getTimestampDifference(getItem(position));
+        if(!timestampDifference.equals("0")){
+            holder.timestamp.setText(timestampDifference + " d");
+        }else{
+            holder.timestamp.setText("today");
+        }
+
+        //set the username
+        
+
+        return convertView;
+    }
+
+    /**
+     * Returns a string representing the number of days ago the post was made
+     * @return
+     */
+    private String getTimestampDifference(Comment comment){
+        Log.d(TAG, "getTimestampDifference: getting timestamp difference.");
+
+        String difference = "";
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.CANADA);
+        sdf.setTimeZone(TimeZone.getTimeZone("Canada/Pacific"));//google 'android list of timezones'
+        Date today = c.getTime();
+        sdf.format(today);
+        Date timestamp;
+        final String photoTimestamp = comment.getDate_created();
+        try{
+            timestamp = sdf.parse(photoTimestamp);
+            difference = String.valueOf(Math.round(((today.getTime() - timestamp.getTime()) / 1000 / 60 / 60 / 24 )));
+        }catch (ParseException e){
+            Log.e(TAG, "getTimestampDifference: ParseException: " + e.getMessage() );
+            difference = "0";
+        }
+        return difference;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
