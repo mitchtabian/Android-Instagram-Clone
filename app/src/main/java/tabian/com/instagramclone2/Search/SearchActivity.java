166fa8c3e02756757cc99020726692010efe98b4
Mitch <mitch@tabian.ca>
2017-09-13 14:11:43 -0700

    part82 - Search Acitivty Layout

--- app/src/main/java/tabian/com/instagramclone2/Search/SearchActivity.java
+++ app/src/main/java/tabian/com/instagramclone2/Search/SearchActivity.java
@@ -7,11 +7,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.List;
 
 import tabian.com.instagramclone2.R;
 import tabian.com.instagramclone2.Utils.BottomNavigationViewHelper;
+import tabian.com.instagramclone2.models.User;
 
 /**
  * Created by User on 5/28/2017.
@@ -23,14 +36,61 @@ public class SearchActivity extends AppCompatActivity{
 
     private Context mContext = SearchActivity.this;
 
+    //widgets
+    private EditText mSearchParam;
+    private ListView mListView;
+
+    //vars
+    private List<User> mUserList;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_search);
         Log.d(TAG, "onCreate: started.");
 
+        hideSoftKeyboard();
         setupBottomNavigationView();
     }
+
+
+    private void searchForMatch(String keyword){
+        Log.d(TAG, "searchForMatch: searching for a match: " + keyword);
+        mUserList.clear();
+        //update the users list view
+        if(keyword.length() ==0){
+
+        }else{
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+            Query query = reference.child(getString(R.string.dbname_users))
+                    .orderByChild(getString(R.string.field_username)).equalTo(keyword);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
+                        Log.d(TAG, "onDataChange: found user:" + singleSnapshot.getValue(User.class).toString());
+
+                        mUserList.add(singleSnapshot.getValue(User.class));
+                        //update the users list view
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+    
+
+    private void hideSoftKeyboard(){
+        if(getCurrentFocus() != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+
 
     /**
      * BottomNavigationView setup
