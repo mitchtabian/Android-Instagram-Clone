819c59caea5d0ecaf38d412126471589a7e3c793
Mitch <mitch@tabian.ca>
2017-09-22 11:55:17 -0700

    part92 - Displaying Posts in the Feed p2

--- app/src/main/java/tabian/com/instagramclone2/Home/HomeFragment.java
+++ app/src/main/java/tabian/com/instagramclone2/Home/HomeFragment.java
@@ -3,11 +3,33 @@ package tabian.com.instagramclone2.Home;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import tabian.com.instagramclone2.R;
+import tabian.com.instagramclone2.Utils.MainfeedListAdapter;
+import tabian.com.instagramclone2.models.Comment;
+import tabian.com.instagramclone2.models.Like;
+import tabian.com.instagramclone2.models.Photo;
+import tabian.com.instagramclone2.models.UserAccountSettings;
 
 /**
  * Created by User on 5/28/2017.
@@ -16,11 +38,128 @@ import tabian.com.instagramclone2.R;
 public class HomeFragment extends Fragment {
     private static final String TAG = "HomeFragment";
 
+    //vars
+    private ArrayList<Photo> mPhotos;
+    private ArrayList<String> mFollowing;
+    private ListView mListView;
+    private MainfeedListAdapter mAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mListView = (ListView) view.findViewById(R.id.listView);
+        mFollowing = new ArrayList<>();
+        mPhotos = new ArrayList<>();
+
+        getFollowing();
 
         return view;
     }
+
+    private void getFollowing(){
+        Log.d(TAG, "getFollowing: searching for following");
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference
+                .child(getString(R.string.dbname_following))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                    Log.d(TAG, "onDataChange: found user: " +
+                    singleSnapshot.child(getString(R.string.field_user_id)).getValue());
+
+                    mFollowing.add(singleSnapshot.child(getString(R.string.field_user_id)).getValue().toString());
+                }
+                //get the photos
+                getPhotos();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getPhotos(){
+        Log.d(TAG, "getPhotos: getting photos");
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        for(int i = 0; i < mFollowing.size(); i++){
+            final int count = i;
+            Query query = reference
+                    .child(getString(R.string.dbname_user_photos))
+                    .child(mFollowing.get(i))
+                    .orderByChild(getString(R.string.field_user_id))
+                    .equalTo(mFollowing.get(i));
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+
+                        Photo photo = new Photo();
+                        Map<String, Object> objectMap = (HashMap<String, Object>) singleSnapshot.getValue();
+
+                        photo.setCaption(objectMap.get(getString(R.string.field_caption)).toString());
+                        photo.setTags(objectMap.get(getString(R.string.field_tags)).toString());
+                        photo.setPhoto_id(objectMap.get(getString(R.string.field_photo_id)).toString());
+                        photo.setUser_id(objectMap.get(getString(R.string.field_user_id)).toString());
+                        photo.setDate_created(objectMap.get(getString(R.string.field_date_created)).toString());
+                        photo.setImage_path(objectMap.get(getString(R.string.field_image_path)).toString());
+
+                        ArrayList<Comment> comments = new ArrayList<Comment>();
+                        for (DataSnapshot dSnapshot : singleSnapshot
+                                .child(getString(R.string.field_comments)).getChildren()){
+                            Comment comment = new Comment();
+                            comment.setUser_id(dSnapshot.getValue(Comment.class).getUser_id());
+                            comment.setComment(dSnapshot.getValue(Comment.class).getComment());
+                            comment.setDate_created(dSnapshot.getValue(Comment.class).getDate_created());
+                            comments.add(comment);
+                        }
+
+                        photo.setComments(comments);
+                        mPhotos.add(photo);
+                    }
+                    if(count >= mFollowing.size() -1){
+                        //display our photos
+                        displayPhotos();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    private void displayPhotos(){
+        if(mPhotos != null){
+            Collections.sort(mPhotos, new Comparator<Photo>() {
+                @Override
+                public int compare(Photo o1, Photo o2) {
+                    return o2.getDate_created().compareTo(o1.getDate_created());
+                }
+            });
+
+            mAdapter = new MainfeedListAdapter(getActivity(), R.layout.layout_mainfeed_listitem, mPhotos);
+            mListView.setAdapter(mAdapter);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/tabian/com/instagramclone2/Utils/MainfeedListAdapter.java
+++ app/src/main/java/tabian/com/instagramclone2/Utils/MainfeedListAdapter.java
@@ -63,6 +63,7 @@ public class MainfeedListAdapter extends ArrayAdapter<Photo> {
         mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         mLayoutResource = resource;
         this.mContext = context;
+        mReference = FirebaseDatabase.getInstance().getReference();
     }
 
     static class ViewHolder{
@@ -101,7 +102,7 @@ public class MainfeedListAdapter extends ArrayAdapter<Photo> {
             holder.comments = (TextView) convertView.findViewById(R.id.image_comments_link);
             holder.caption = (TextView) convertView.findViewById(R.id.image_caption);
             holder.timeDetla = (TextView) convertView.findViewById(R.id.image_time_posted);
-            holder.mprofileImage = (CircleImageView) convertView.findViewById(R.id.profile_image);
+            holder.mprofileImage = (CircleImageView) convertView.findViewById(R.id.profile_photo);
             holder.heart = new Heart(holder.heartWhite, holder.heartRed);
             holder.photo = getItem(position);
             holder.detector = new GestureDetector(mContext, new GestureListener(holder));
@@ -203,6 +204,29 @@ public class MainfeedListAdapter extends ArrayAdapter<Photo> {
                             //another thing?
                         }
                     });
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        //get the user object
+        Query userQuery = mReference
+                .child(mContext.getString(R.string.dbname_users))
+                .orderByChild(mContext.getString(R.string.field_user_id))
+                .equalTo(getItem(position).getUser_id());
+        userQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                    Log.d(TAG, "onDataChange: found user: " +
+                    singleSnapshot.getValue(User.class).getUsername());
+
+                    holder.user = singleSnapshot.getValue(User.class);
                 }
 
             }
