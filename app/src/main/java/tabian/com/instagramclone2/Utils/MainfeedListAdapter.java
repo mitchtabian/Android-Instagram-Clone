2a5f8f2f7eb018fc40464b00aa5d220c988d10ae
Mitch <mitch@tabian.ca>
2017-09-22 10:33:54 -0700

    part89 - Mainfeed ListAdapter

---
+++ app/src/main/java/tabian/com/instagramclone2/Utils/MainfeedListAdapter.java
@@ -0,0 +1,204 @@
+package tabian.com.instagramclone2.Utils;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import tabian.com.instagramclone2.R;
+import tabian.com.instagramclone2.models.Like;
+import tabian.com.instagramclone2.models.Photo;
+import tabian.com.instagramclone2.models.User;
+import tabian.com.instagramclone2.models.UserAccountSettings;
+
+/**
+ * Created by User on 9/22/2017.
+ */
+
+public class MainfeedListAdapter extends ArrayAdapter<Photo> {
+    private static final String TAG = "MainfeedListAdapter";
+
+    private LayoutInflater mInflater;
+    private int mLayoutResource;
+    private Context mContext;
+    private DatabaseReference mReference;
+    private String currentUsername = "";
+
+    public MainfeedListAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Photo> objects) {
+        super(context, resource, objects);
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mLayoutResource = resource;
+        this.mContext = context;
+    }
+
+    static class ViewHolder{
+        CircleImageView mprofileImage;
+        String likesString;
+        TextView username, timeDetla, caption, likes, comments;
+        SquareImageView image;
+        ImageView heartRed, heartWhite, comment;
+
+        UserAccountSettings settings = new UserAccountSettings();
+        User user  = new User();
+        StringBuilder users;
+        String mLikesString;
+        boolean likeByCurrentUser;
+        Heart heart;
+        GestureDetector detector;
+        Photo photo;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        final ViewHolder holder;
+
+        if(convertView == null){
+            convertView = mInflater.inflate(mLayoutResource, parent, false);
+            holder = new ViewHolder();
+
+            holder.username = (TextView) convertView.findViewById(R.id.username);
+            holder.image = (SquareImageView) convertView.findViewById(R.id.post_image);
+            holder.heartRed = (ImageView) convertView.findViewById(R.id.image_heart_red);
+            holder.heartWhite = (ImageView) convertView.findViewById(R.id.image_heart);
+            holder.comment = (ImageView) convertView.findViewById(R.id.speech_bubble);
+            holder.likes = (TextView) convertView.findViewById(R.id.image_likes);
+            holder.comments = (TextView) convertView.findViewById(R.id.image_comments_link);
+            holder.caption = (TextView) convertView.findViewById(R.id.image_caption);
+            holder.timeDetla = (TextView) convertView.findViewById(R.id.image_time_posted);
+            holder.mprofileImage = (CircleImageView) convertView.findViewById(R.id.profile_image);
+            holder.heart = new Heart(holder.heartWhite, holder.heartRed);
+            holder.photo = getItem(position);
+            holder.detector = new GestureDetector(mContext, new GestureListener(holder));
+            holder.users = new StringBuilder();
+
+        }
+
+
+        return convertView;
+    }
+
+    public class GestureListener extends GestureDetector.SimpleOnGestureListener{
+
+        ViewHolder mHolder;
+        public GestureListener(ViewHolder holder) {
+            mHolder = holder;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Log.d(TAG, "onDoubleTap: double tap detected.");
+
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+            Query query = reference
+                    .child(mContext.getString(R.string.dbname_photos))
+                    .child(mHolder.photo.getPhoto_id())
+                    .child(mContext.getString(R.string.field_likes));
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+
+                        String keyID = singleSnapshot.getKey();
+
+                        //case1: Then user already liked the photo
+                        if(mHolder.likeByCurrentUser &&
+                                singleSnapshot.getValue(Like.class).getUser_id()
+                                        .equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+
+                            mReference.child(mContext.getString(R.string.dbname_photos))
+                                    .child(mHolder.photo.getPhoto_id())
+                                    .child(mContext.getString(R.string.field_likes))
+                                    .child(keyID)
+                                    .removeValue();
+///
+                            mReference.child(mContext.getString(R.string.dbname_user_photos))
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .child(mHolder.photo.getPhoto_id())
+                                    .child(mContext.getString(R.string.field_likes))
+                                    .child(keyID)
+                                    .removeValue();
+
+                            mHolder.heart.toggleLike();
+                            getLikesString();
+                        }
+                        //case2: The user has not liked the photo
+                        else if(!mHolder.likeByCurrentUser){
+                            //add new like
+                            addNewLike();
+                            break;
+                        }
+                    }
+                    if(!dataSnapshot.exists()){
+                        //add new like
+                        addNewLike();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+            return true;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
